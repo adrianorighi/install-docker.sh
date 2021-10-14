7329fd6b70f3aa93590a31cbd2a130bba9f94fd9
Adriano Righi <adrianorighi@users.noreply.github.com>
2021-10-14 09:47:37 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#--------------------------------------------------------#
+# Shell Script to auto install Docker and docker-compose #
+#            Run on Ubuntu 18.04 or Later                #
+#              Created by Adriano Righi                  #
+#                  adrianorighi.com                      #
+#--------------------------------------------------------#
+
+# Script only works if sudo caches the password for a few minutes
+sudo true
+
+# Install Docker
+wget -qO- https://get.docker.com/ | sh
+
+# Install docker-compose
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Install docker-cleanup command
+cd /tmp
+git clone https://gist.github.com/76b450a0c986e576e98b.git
+cd 76b450a0c986e576e98b
+sudo mv docker-cleanup /usr/local/bin/docker-cleanup
+sudo chmod +x /usr/local/bin/docker-cleanup
+
+# Add user to docker group to run without sudo
+sudo usermod -aG docker $USER
+
+dialog --backtitle "Auto Install Docker and Docker-Compose" --title "Sucesso!" --clear --msgbox "Instalação concluida com sucesso!" 8 40
